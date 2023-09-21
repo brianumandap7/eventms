5d3ddd274c20976b4820c3f65cb43708c5170011
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2023-09-21 21:07:05 +0800

    21 09

--- login/urls.py
+++ login/urls.py
@@ -11,7 +11,7 @@ app_name = 'login'
 
 urlpatterns = [
      path('', auth_views.LoginView.as_view(template_name='login/login.html', redirect_authenticated_user = True, authentication_form=CustomAuthForm), name = 'login-login'),
-     path('logout/', auth_views.LogoutView.as_view(template_name='login/login.html'), name = 'login-logout'),
+     path('logout/', auth_views.LogoutView.as_view(template_name='login/logout.html'), name = 'login-logout'),
 ]
 
 if settings.DEBUG:
